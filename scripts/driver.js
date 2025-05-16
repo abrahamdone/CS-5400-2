9243771f52ba6f47e030a2fdb9e122cb7c51a2ec
Abraham Done <andone@mac.com>
2025-05-16 16:30:57 -0600

    Animation improvements

--- scripts/driver.js
+++ scripts/driver.js
@@ -8,7 +8,7 @@ MySample.main = (function(graphics) {
     let controlOne = {x: 0.2, y: -0.5};
     let controlTwo = {x: -0.2, y: 0.5};
     let segments = 100;
-    let startHue = 0;
+    let nextHue = 0;
     let colors = [];
     let curve = graphics.Curve.Hermite;
     let display = 0.0;
@@ -27,6 +27,7 @@ MySample.main = (function(graphics) {
             }
         }
 
+        // Display each curve for a period of time
         if (display < displayMultiplier) {
             curve = graphics.Curve.Hermite;
             display += elapsedTime;
@@ -37,36 +38,31 @@ MySample.main = (function(graphics) {
             curve = graphics.Curve.Bezier;
             display += elapsedTime;
         } else if (display < 4 * displayMultiplier) {
-            colors.length = 0;
-            startHue += 1;
-            if (startHue > 360) {
-                startHue = 0;
+            // Animate Hue
+            nextHue += 1;
+            if (nextHue > 360) {
+                nextHue = 0;
             }
-            let nextHue = startHue;
-            for (let i = 0; i < segments; i += 1) {
-                colors.push(`hsl(${nextHue}, 80%, 50%)`);
-                if (nextHue > 360) {
-                    nextHue = 0;
-                }
-                nextHue += 1;
-            }
+            colors.shift();
+            colors.push(`hsl(${nextHue}, 80%, 50%)`)
 
-            let random0 = (Math.floor(Math.random() * 3) - 1) * 0.005;
-            let random1 = (Math.floor(Math.random() * 3) - 1) * 0.005;
-            let random2 = (Math.floor(Math.random() * 3) - 1) * 0.005;
-            let random3 = (Math.floor(Math.random() * 3) - 1) * 0.005;
+            // move the control point by a small amount in a random direction
+            let randomOneX = (Math.floor(Math.random() * 3) - 1) * 0.005;
+            let randomOneY = (Math.floor(Math.random() * 3) - 1) * 0.005;
+            let randomTwoX = (Math.floor(Math.random() * 3) - 1) * 0.005;
+            let randomTwoY = (Math.floor(Math.random() * 3) - 1) * 0.005;
             controlOne = {
-                x: Math.max(-1, Math.min(1, controlOne.x + random0)),
-                y: Math.max(-1, Math.min(1, controlOne.y + random1))
+                x: Math.max(-1, Math.min(1, controlOne.x + randomOneX)),
+                y: Math.max(-1, Math.min(1, controlOne.y + randomOneY))
             };
             controlTwo = {
-                x: Math.max(-1, Math.min(1, controlTwo.x + random2)),
-                y: Math.max(-1, Math.min(1, controlTwo.y + random3))
+                x: Math.max(-1, Math.min(1, controlTwo.x + randomTwoX)),
+                y: Math.max(-1, Math.min(1, controlTwo.y + randomTwoY))
             };
         }
     }
 
-    function convertWorldTODevice(point) {
+    function convertWorldToDevice(point) {
         return {
             x: math.floor(graphics.sizeX / 2 + point.x * graphics.sizeX / 2),
             y: math.floor(graphics.sizeY / 2 + point.y * graphics.sizeY / 2)
@@ -83,10 +79,10 @@ MySample.main = (function(graphics) {
         graphics.drawCurve(
             curve,
             {
-                start: convertWorldTODevice(start),
-                end: convertWorldTODevice(end),
-                controlOne: convertWorldTODevice(controlOne),
-                controlTwo: convertWorldTODevice(controlTwo),
+                start: convertWorldToDevice(start),
+                end: convertWorldToDevice(end),
+                controlOne: convertWorldToDevice(controlOne),
+                controlTwo: convertWorldToDevice(controlTwo),
                 tension: 0
             },
             colors,
